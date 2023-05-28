650bae975e7c29c89bbb68d3ceb72b07b415e929
vandreas73 <113762114+vandreas73@users.noreply.github.com>
2023-05-28 18:58:12 +0200

    restructure project, remove unnecessary files

--- Graphic runner/Graphic_runner.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM Futtatas:
-java -cp Vilagtalan_virologusok\forditott\;forms_rt.jar vilagtalan_virologusok.Controller
--- Skeleton runner/Skeleton_runner.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM Futtatas:
-java -cp Vilagtalan_virologusok\forditott\ vilagtalan_virologusok.Controller
